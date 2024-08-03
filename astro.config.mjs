637d18723b75c3a5c76f9a761225495ea7ccfdd7
David Brown <2047439+dugbus@users.noreply.github.com>
2024-08-03 08:17:08 +0100

    Added films-of-scotland redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://syff.scot',
+  redirects: {
+    '/films-of-scotland': '/filmsofscotland'
+  },
   integrations: [sitemap()]
 });
